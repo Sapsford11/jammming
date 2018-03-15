6c4cc6ff120728cada78d8f7cf42512fd0635037
Sapsford11 <craig.sapsford11@gmail.com>
2018-03-15 15:48:50 +0100

    Updated upto step 84

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,22 @@
+const clientID = '9dc421eaefbd4e6195484248a7ba4d25';
+const redirectUri = "http://localhost:3000/";
 let accesstoken = '';
 
 Spotify {
   getAccessToken() {
     if(accessToken){
       return accessToken;
+    }
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      let expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/'); // clears the parameters, allows to grab a new access token when expires
+      return accessToken;
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
     }
 };
 
