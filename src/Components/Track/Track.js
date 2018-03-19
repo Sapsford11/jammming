65db08babebe0bef64f483580659506f40586032
Sapsford11 <craig.sapsford11@gmail.com>
2018-03-19 15:02:11 +0100

    Fixed renderAction method

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -11,13 +11,12 @@ class Track extends React.Component {
   /* Adds functionality to the '-' and '+' buttons, the methods
   addTrack and removeTrack are described below. */
   renderAction() {
-    if (this.props.isRemoval)
-    return (
-      <a className="Track-action" onClick={this.removeTrack}>-</a>
-    )
-      else return (
-        <a className="Track-action" onClick={this.addTrack}>+</a>
-      )
+    if (this.props.isRemoval) {
+    return <a className="Track-action" onClick={this.removeTrack}>-</a>
+    }
+      else {
+        return <a className="Track-action" onClick={this.addTrack}>+</a>
+      }
 };
 
 addTrack() {
